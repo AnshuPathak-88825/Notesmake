2bb3662cc1d29b5c41173d981c62d1a305dfd876
AnshuPathak-88825 <shubhnanshupathak88825@gmail.com>
2022-03-24 16:33:53 +0530

    sign in and sign out view created

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -2,3 +2,11 @@ module.exports.profile=function(req,res)
 {
     return res.render('profile');
 }
+module.exports.signup=function(req,res)
+{
+    return res.render('user_sign_up',{title:'signup'});
+}
+module.exports.signin=function(req,res)
+{
+    return res.render('user_sign_in',{title:'signin'});
+}
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,8 @@ const router=express.Router();
 const homecontroller=require('../controller/usercontroller');
 
 router.get('/profile',homecontroller.profile);
+router.get('/signup',homecontroller.signup);
+router.get('/signin',homecontroller.signin);
+
 
 module.exports=router;
