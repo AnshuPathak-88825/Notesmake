3b3978cc30f6e101b0f0a7030ad55507bc12c817
AnshuPathak-88825 <shubhnanshupathak88825@gmail.com>
2022-03-29 16:40:56 +0530

    cookie module added successfully

--- controller/homecontroller.js
+++ controller/homecontroller.js
@@ -1,4 +1,5 @@
 module.exports.home=function(req,res)
 {
+    console.log(req.cookies);
     return res.render('home');
 }
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //getting express package
 const express=require('express');
 // express is function 
+const cookieParser=require('cookie-parser');
 const app=express();
 const port=9000;
 
 const db=require('./config/mogooose');
-app.use(express.urlencoded());
+app.use(express.urlencoded());  
+app.use(cookieParser());
 
-// using homecontroller
 
-
-app.use('/',require('./routes'));//mod to get reques from index.js
 
 app.set('view engine','ejs');
 app.set('views','./views');
+app.use('/',require('./routes'));
 
 app.listen(port,function(error){
     if(error)
