3c13cb20a72ed909c2719018de6f0591fa188a09
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2022-04-10 00:32:47 +0530

    schema of post created

---
+++ model/post.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose');
+const postSchema=new mongoose.Schema(
+    {
+        content:{
+            type:String,
+            required:true
+        },
+        Title:{
+            type:String,
+            required:true
+        },
+        user:{
+
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }
+
+    },{
+        timestamps:true
+    }
+);
+
+
+const Post=mongoose.model('Post',postSchema);
+module.exports =Post;
