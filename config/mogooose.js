a231a6491dbf3b1b1f2287e51ed3a078db0ee6aa
Anshu Pathak <78332454+AnshuPathak-88825@users.noreply.github.com>
2022-05-15 20:45:39 +0530

    Update mogooose.js

--- config/mogooose.js
+++ config/mogooose.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const DBURL2= 'mongodb://127.0.0.1/notemake'
 const DBURL = 'mongodb+srv://Anshu:SbJm5Wkx7TVmHEeJ@cluster0.4f56l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(DBURL2);
+mongoose.connect(DBURL);
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,"error connecting to db"));
 db.once('open',function(){
